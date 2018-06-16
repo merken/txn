ebc175dd7f0466b9d81165ecc8801536215866d4
Maarten Merken <merken.maarten@gmail.com>
2018-06-16 06:35:28 +0200

    Use IAsyncActionFilter

--- txn/UnitOfWorkFilter.cs
+++ txn/UnitOfWorkFilter.cs
@@ -4,14 +4,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace txn {
-    public class UnitOfWorkFilter : ActionFilterAttribute {
+    public class UnitOfWorkFilter : IAsyncActionFilter {
         private readonly IDbTransaction transaction;
 
         public UnitOfWorkFilter (IDbTransaction transaction) {
             this.transaction = transaction;
         }
 
-        public override async Task OnActionExecutionAsync (ActionExecutingContext executingContext, ActionExecutionDelegate next) {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next) {
             var connection = transaction.Connection;
             if (connection.State != ConnectionState.Open)
                 throw new NotSupportedException ("The provided connection was not open!");
